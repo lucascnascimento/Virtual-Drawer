33f5ebf14a9d3fc604e0348bb90aed0ce4bac8aa
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-07-30 14:38:41 -0300

    Install eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
