b5084b88af0eb9039ee5891a5904cd2f54d63890
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-07-31 09:26:16 -0300

    Create a basic folder strucuture

---
+++ src/components/ListContainer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const ListContainer = () => {
+  return <View />;
+};
+
+export default ListContainer;
