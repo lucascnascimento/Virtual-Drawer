03d01753a5081153fac01a408482f241540ac1d8
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-01 15:23:11 -0300

    Fix import eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,13 @@ module.exports = {
   env: {
     es6: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/react',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -16,13 +22,14 @@ module.exports = {
   },
   plugins: [
     'react',
-    '@typescript-eslint',
     'react-hooks',
+    '@typescript-eslint',
     'jsx-a11y',
     'import',
     'prettier',
   ],
   rules: {
+    'prettier/prettier': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': [
@@ -30,10 +37,14 @@ module.exports = {
       { extensions: ['.js', '.jsx', '.tsx'] },
     ],
     'react/jsx-one-expression-per-line': 'off',
+    'import/extensions': [0, 'never'],
+    'import/no-unresolved': 0,
   },
   settings: {
     'import/resolver': {
-      'babel-plugin-root-import': { rootPathSuffix: 'src' },
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
     },
   },
 };
