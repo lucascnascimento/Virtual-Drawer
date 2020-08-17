cc4ee6c180445e3dd67500237743ca52a51f37ab
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-17 11:54:25 -0300

    Add translation file and functions

---
+++ src/translations/en.js
@@ -0,0 +1,23 @@
+export default {
+  drawers: 'Drawers',
+  'enter the drawer name': 'Enter the drawer name',
+  cancel: 'Cancel',
+  create: 'Create',
+  'drawer name placeholder': 'Drawer name',
+  'search for an item': 'Search for an item',
+  name: 'Name',
+  description: 'Description',
+  location: 'Location',
+  createdAt: 'Created at',
+  updateAt: 'Updated at',
+  'save changes question': 'Do you want to save the changes?',
+  'save item body':
+    'By clicking on save a new item will be added to your drawer',
+  'save drawer body': 'By clicking on save a new drawer will be created',
+  'dont show this message again': "Don't show this message again",
+  'attention item body':
+    'Attention! Changed items cannot be reversed, are you sure you want to modify them?',
+  'new item': 'New item',
+  'no description': 'No description',
+  add: 'add',
+};
---
+++ src/translations/pt.js
@@ -0,0 +1,23 @@
+export default {
+  drawers: 'Gavetas',
+  'enter the drawer name': 'Dê um nome para a gaveta',
+  cancel: 'Cancelar',
+  create: 'Criar',
+  'drawer name placeholder': 'Nome da gaveta',
+  'search for an item': 'Procurar por um item',
+  name: 'Nome',
+  description: 'Descrição',
+  location: 'Local',
+  createdAt: 'Criado em',
+  updateAt: 'Modificado em',
+  'save changes question': 'Gostaria de salvar as alterações?',
+  'save item body':
+    'Ao clicar em salvar, um novo item será adicionado a sua gaveta',
+  'save drawer body': 'Ao clicar em salvar, uma nova gaveta será criada',
+  'dont show this message again': 'Não mostrar esta mensagem novamente',
+  'attention item body':
+    'Atenção! Items alterados não poderão ser revertidos, tem certeza que quer modificá-los?',
+  'new item': 'Novo item',
+  'no description': 'Sem descrição',
+  add: 'adicionar',
+};
