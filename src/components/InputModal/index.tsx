2b01ba449c61040e57777777b260970628a4f679
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-18 13:44:22 -0300

    Create input modal structure and style

---
+++ src/components/InputModal/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { View, Modal, Alert, Text, StyleSheet, TextInput } from 'react-native';
+import translate from '~/translations';
+
+import {
+  Container,
+  Header,
+  Title,
+  Body,
+  Input,
+  Footer,
+  Button,
+  ModalView,
+} from './styles';
+
+interface InputModalProps {
+  visible: boolean;
+  setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const InputModal = ({ visible, setModalVisible }: InputModalProps) => {
+  const [inputText, setInputText] = useState('');
+  const [inputError, setInputError] = useState(false);
+
+  const regex = new RegExp('^[a-zA-Z0-9]+$');
+
+  /**
+   * Handle and validates the input text
+   * @param text Text written on input
+   */
+  function handleTextChange(text: string) {
+    setInputText(text);
+    if (!regex.test(text)) {
+      setInputError(true);
+    } else if (text.length < 3) {
+      setInputError(true);
+    } else {
+      setInputError(false);
+    }
+  }
+
+  function handleSubmit() {
+    // Chama a action que cria o db
+    console.log('peido');
+  }
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent
+      visible={visible}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+      }}>
+      <Container>
+        <ModalView style={styles.boxShadow}>
+          <Header>
+            <Title>{translate('enter the drawer name')}</Title>
+          </Header>
+          <Body>
+            <Input
+              inputError={inputError}
+              value={inputText}
+              onChangeText={handleTextChange}
+              placeholder="Gaveta"
+              returnKeyType="send"
+              onSubmitEditing={handleSubmit}
+            />
+          </Body>
+          <Footer>
+            <Button
+              onPress={() => {
+                setModalVisible(false);
+              }}>
+              <Text>{translate('cancel')}</Text>
+            </Button>
+            <Button
+              onPress={handleSubmit}
+              disabled={!inputError}
+              style={inputError && { opacity: 0.5 }}>
+              <Text>{translate('create')}</Text>
+            </Button>
+          </Footer>
+        </ModalView>
+      </Container>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default InputModal;
+
+// todo: validar se as tabelas não tem o mesmo nome
---
+++ src/components/InputModal/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalView = styled.View`
+  margin: 20px;
+  padding: 35px;
+  border-radius: 20px;
+  background: white;
+`;
+
+export const Header = styled.View`
+  padding-bottom: 12px;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+`;
+
+export const Body = styled.View`
+  padding: 12px 0px;
+`;
+
+export const Input = styled.TextInput`
+  border-bottom-width: 1px;
+  padding-bottom: 0px;
+  width: 220px;
+  border-bottom-color: ${(props) => (props.inputError ? '#f00' : '#000')};
+`;
+
+export const Footer = styled.View`
+  padding-top: 12px;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const Button = styled.TouchableOpacity`
+  padding: 0px 4px;
+  color: #2196f3;
+`;
