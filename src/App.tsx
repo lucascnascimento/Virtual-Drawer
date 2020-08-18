0cbabfc92bdb455dd823f3f0eee6d629228e3e77
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-18 15:14:27 -0300

    Create navigation container and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SLIIcons from 'react-native-vector-icons/SimpleLineIcons';
+import translate from '~/translations';
 
-const App = () => (
-  <View>
-    <Text>Olá mundo, App.tsx</Text>
-  </View>
-);
+import Home from '~/screens/Home';
+import SearchAndAddIcons from '~/components/SearchAndAddIcons';
+
+import { configStore } from '~/store';
+
+const { store } = configStore();
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: translate('drawers'),
+              headerRight: () => <SearchAndAddIcons />,
+              headerLeft: () => (
+                <SLIIcons name="settings" color="#000" size={24} />
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
 
 export default App;
