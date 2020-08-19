c3f2bbc6304783519ad26d342239becce7cdc69b
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-19 15:20:25 -0300

    Change the gesture handlers for a swipable component and add the trash button

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,46 +1,34 @@
-import React, { useRef } from 'react';
-import { Text, View } from 'react-native';
-import {
-  LongPressGestureHandler,
-  State,
-  TapGestureHandler,
-  TouchableOpacity,
-} from 'react-native-gesture-handler';
+import React from 'react';
+import { Alert, Animated, Text, View } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import MCIIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import SLIcon from 'react-native-vector-icons/SimpleLineIcons';
 import { TableName } from '~/types/types';
 
-import { Container, Icon, Body, BodyTitle, Trash } from './styles';
+import { Container, Icon, Body, BodyTitle, TrashButton } from './styles';
 
 type ListItemProps = { item: TableName };
 
 const ListItem: React.FC = ({ item }: ListItemProps) => {
-  function handleTap({ nativeEvent }) {
-    nativeEvent.state === State.END && console.log('Short Press');
-  }
-
-  function handleLongPress({ nativeEvent }) {
-    nativeEvent.state === State.END && console.log('Looooong Press');
+  function renderRightAction() {
+    return (
+      <TrashButton onPress={() => console.log('lixo')}>
+        <SLIcon name="trash" size={24} color="#fff" />
+      </TrashButton>
+    );
   }
 
   return (
-    <TapGestureHandler onHandlerStateChange={handleTap}>
-      <LongPressGestureHandler
-        onHandlerStateChange={handleLongPress}
-        minDurationMs={500}>
-        <Container>
-          <Icon>
-            <MCIIcon name="file-cabinet" size={32} color="#000" />
-          </Icon>
-          <Body>
-            <BodyTitle>{item.name}</BodyTitle>
-          </Body>
-          <Trash>
-            <SLIcon name="trash" size={24} color="#FFF" />
-          </Trash>
-        </Container>
-      </LongPressGestureHandler>
-    </TapGestureHandler>
+    <Swipeable renderRightActions={renderRightAction}>
+      <Container onPress={() => console.log('pressionado')}>
+        <Icon>
+          <MCIIcon name="file-cabinet" size={32} color="#000" />
+        </Icon>
+        <Body>
+          <BodyTitle>{item.name}</BodyTitle>
+        </Body>
+      </Container>
+    </Swipeable>
   );
 };
 
--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+export const Container = styled(TouchableOpacity)`
   flex-direction: row;
   padding: 20px;
-  justify-content: space-between;
-  background-color: #ccc;
-  margin: 5px;
+  justify-content: flex-start;
+  background-color: #fff;
   align-items: center;
 `;
 
@@ -13,8 +13,14 @@ export const Icon = styled.View``;
 
 export const Body = styled.View``;
 
-export const BodyTitle = styled.Text``;
+export const BodyTitle = styled.Text`
+  font-size: 22px;
+  padding-left: 20px;
+`;
 
-export const Trash = styled.View`
+export const TrashButton = styled.TouchableOpacity`
   background: #f00;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 20px;
 `;
