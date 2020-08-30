a2a6ba4b72c10631cfab1a549df4d70c45b18de9
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-29 21:36:55 -0300

    Add documentation to searchTable

--- src/screens/SearchTable/index.tsx
+++ src/screens/SearchTable/index.tsx
@@ -3,8 +3,17 @@ import Search from '~/components/Search';
 import db from '~/database/db';
 import { SearchTableProps } from '~/types/types';
 
+/**
+ * Container for search tables
+ */
 const SearchTable: React.FC<SearchTableProps> = () => {
-  return <Search queryFunction={db.selectTable} deleteItem={db.dropTable} />;
+  return (
+    <Search
+      queryFunction={db.selectTable}
+      deleteItem={db.dropTable}
+      parent="SearchTable"
+    />
+  );
 };
 
 export default SearchTable;
