f414b45233d1ee31f2a0633467cb3e1385e43867
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-08-31 10:52:24 -0300

    Create selectTable, selectAllRows, insertRow, updateRow, deleteRow, searchRows and selectRow queries

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,6 +5,8 @@ import {
   SQLiteDatabase,
 } from 'react-native-sqlite-storage';
 
+import { Item } from '~/types/types';
+
 /**
  * Opens the connection to the database
  */
@@ -23,13 +25,13 @@ const db: SQLiteDatabase = openDatabase(
  * @param sql SQL query to be executed
  * @param params SQL query parameters
  */
-function executeQuery(sql: string, params = []): Promise<ResultSet> {
+function executeQuery(sql: string, params = [] as any): Promise<ResultSet> {
   return new Promise((resolve, reject) => {
     db.transaction((trans) => {
       trans.executeSql(
         sql,
         params,
-        (trans, results) => {
+        (_, results) => {
           resolve(results);
         },
         (error) => {
@@ -102,4 +104,83 @@ function selectTable(searchField: string): Promise<ResultSet> {
     [],
   );
 }
-export default { indexTables, createTable, dropTable, selectTable };
+
+/**
+ * Select all rows from the specified table
+ * @param tableName Table in which the rows will be selected
+ */
+function selectAllRows(tableName: string): Promise<ResultSet> {
+  return executeQuery(`SELECT * FROM ${tableName}`, []);
+}
+
+/**
+ * Inserts a row in the selected table
+ * @param tableName Table in which the information will be inserted
+ * @param item Item to be inserted
+ */
+function insertRow(tableName: string, item: Item): Promise<ResultSet> {
+  return executeQuery(
+    `INSERT INTO ${tableName} (name, description, location, picture_src, created_at, updated_at) VALUES (?, ?, ?, ?, datetime('now'), datetime('now'))`,
+    [item.name, item.description, item.location, item.pictureSrc],
+  );
+}
+
+/**
+ * Updates a row in the selected table
+ * @param tableName Table in which the information will be updated
+ * @param item Item to be updated
+ */
+function updateRow(tableName: string, item: Item): Promise<ResultSet> {
+  return executeQuery(
+    `UPDATE ${tableName} SET name = ?, description = ?, location = ?, picture_src = ?, updated_at = datetime('now') WHERE id = ?`,
+    [item.name, item.description, item.location, item.pictureSrc, item.id],
+  );
+}
+
+/**
+ * Delets a row in the selected table
+ * @param tableName Table in which the infomation will be deleted
+ * @param id Id of the item that will be deleted
+ */
+function deleteRow(tableName: string, id: number): Promise<ResultSet> {
+  return executeQuery(`DELETE FROM ${tableName} WHERE id = ?`, [id]);
+}
+
+/**
+ * Search for an input pattern in a filtered column of a table
+ * @param tableName Table in which the information will be searched
+ * @param searchField Pattern to be searched
+ * @param filter Filter maps to the a column in the table
+ */
+function searchRows(
+  tableName: string,
+  searchField: string,
+  filter: string,
+): Promise<ResultSet> {
+  return executeQuery(
+    `SELECT ${filter} FROM ${tableName} WHERE ${filter} LIKE %${searchField}%`,
+    [],
+  );
+}
+
+/**
+ * Selects a row in the database
+ * @param tableName Table in which information will be selected
+ * @param id Id of the item that will be selected
+ */
+function selectRow(tableName: string, id: number): Promise<ResultSet> {
+  return executeQuery(`SELECT * FROM ${tableName} WHERE id = ?`, [id]);
+}
+
+export default {
+  indexTables,
+  createTable,
+  dropTable,
+  selectTable,
+  selectAllRows,
+  insertRow,
+  updateRow,
+  deleteRow,
+  searchRows,
+  selectRow,
+};
