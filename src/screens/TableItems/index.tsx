6647f8181e520f64447e57e449e9f4ebc1763b96
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-10-22 10:08:20 -0300

    Load Table rows on focus

--- src/screens/TableItems/index.tsx
+++ src/screens/TableItems/index.tsx
@@ -1,20 +1,54 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from 'react';
+import { View } from 'react-native';
 import db from '~/database/db';
+import translate from '~/translations';
 
 import AddButton from '~/components/AddButton';
 import HeaderRightIcons from '~/components/HeaderRightIcons';
 import SearchButton from '~/components/SearchButton';
 
-import { TableItemsProps } from '~/types/types';
-
-// import { Container } from './styles';
+import { Item, TableItemsProps } from '~/types/types';
+import ListContainer from '~/components/ListContainer';
+import Toast from '~/components/Toast';
+import ListItem from '~/components/ListItem';
 
 const TableItems: React.FC<TableItemsProps> = (props: TableItemsProps) => {
   const { route } = props;
 
+  const [itemsList, setItemsList] = useState<Array<Item>>([]);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
+
+  // Load table items on focus
+  useFocusEffect(
+    useCallback(() => {
+      async function loadTableRows() {
+        const res = await db.selectAllRows(route.params.tableName);
+        const { rows } = res;
+        const itemsArray: Item[] = [];
+        rows.raw().forEach((item) => {
+          itemsArray.push({
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            location: item.location,
+            pictureSrc: item.picture_src,
+            createdAt: item.created_at,
+            updatedAt: item.updated_at,
+          });
+        });
+        setItemsList(itemsArray);
+        setLoading(false);
+      }
+      loadTableRows();
+    }, [route.params.tableName]),
+  );
 
   /**
    * Set the right header icons and defines action for handling the add table function
@@ -25,7 +59,10 @@ const TableItems: React.FC<TableItemsProps> = (props: TableItemsProps) => {
      * @param inputText Database table name
      */
     async function handleAddAction(inputText: string) {
-      navigation.navigate('AddItem');
+      console.info(`AddItem tableName: ${route.params.tableName}`);
+      navigation.navigate('AddItem', {
+        pictureSrcFolder: route.params.tableName,
+      });
     }
 
     navigation.setOptions({
@@ -43,9 +80,26 @@ const TableItems: React.FC<TableItemsProps> = (props: TableItemsProps) => {
         />
       ),
     });
-  }, [navigation]);
+  }, [navigation, route.params.tableName]);
 
-  return <Text>Drawer Items: {route.params.tableName}</Text>;
+  return (
+    <View accessibilityLabel="TableItems screen">
+      <ListContainer
+        loading={loading}
+        list={itemsList}
+        renderItem={({ item }) => (
+          <ListItem
+            item={item}
+            trashButtonAction={() => {
+              console.log('trash button');
+            }}
+            navigateTo="TableItems"
+          />
+        )}>
+        <Toast visible={false} message={translate('drawerDeleted')} />
+      </ListContainer>
+    </View>
+  );
 };
 
 export default TableItems;
