94dff153b890b025034037f1930d2b9ee96a3b44
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-10-23 18:55:09 -0300

    Refacto ListItem component to receive a TableName type of data or  either an Item type of data.

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,27 +1,28 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
-import MCIIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import SLIcon from 'react-native-vector-icons/SimpleLineIcons';
 
-import { TableName } from '~/types/types';
+import { Item, TableName } from '~/types/types';
 
-import { Container, Icon, Body, BodyTitle, TrashButton } from './styles';
+import { Container, Body, BodyTitle, TrashButton } from './styles';
 
 type ListItemProps = {
-  item: TableName;
-  trashButtonAction: (name: string) => void;
+  item: TableName | Item;
+  trashButtonAction: (name?: string, id?: number) => void;
   navigateTo: string;
+  icon: React.ReactNode;
 };
 
 /**
  * Renders an item information
- * @param item Item to be rendered
- * @param trashButtonAction Action to be performed by the trash button
- * @param navigateTo Name of the screen that will be navigated to when component is pressed
+ * @param item Item to be rendered.
+ * @param trashButtonAction Action to be performed by the trash button.
+ * @param navigateTo Name of the screen that will be navigated to when component is pressed.
+ * @param icon Icon to be displayed on the left side of the component.
  */
 const ListItem: React.FC<ListItemProps> = (props: ListItemProps) => {
-  const { item, trashButtonAction, navigateTo } = props;
+  const { item, trashButtonAction, navigateTo, icon } = props;
   const navigation = useNavigation();
 
   /**
@@ -29,7 +30,7 @@ const ListItem: React.FC<ListItemProps> = (props: ListItemProps) => {
    */
   function renderRightAction() {
     return (
-      <TrashButton onPress={() => trashButtonAction(item.name)}>
+      <TrashButton onPress={() => trashButtonAction(item.name, item.id)}>
         <SLIcon name="trash" size={24} color="#fff" />
       </TrashButton>
     );
@@ -41,9 +42,7 @@ const ListItem: React.FC<ListItemProps> = (props: ListItemProps) => {
         onPress={() =>
           navigation.navigate(navigateTo, { tableName: item.name })
         }>
-        <Icon>
-          <MCIIcon name="file-cabinet" size={32} color="#000" />
-        </Icon>
+        {icon}
         <Body>
           <BodyTitle>{item.name}</BodyTitle>
         </Body>
